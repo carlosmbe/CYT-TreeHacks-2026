1b2a4dd58a0137e233aeb61cfd82f7b0402ebf77
Carlos Mbendera <carlosmbendera@gmail.com>
2026-02-14 00:09:05 -0800

    Initial Commit

---
+++ CYT/CYTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CYTApp.swift
+//  CYT
+//
+//  Created by Carlos Mbendera on 14/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CYTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
