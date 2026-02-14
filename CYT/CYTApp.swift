957a8360d5572cf3c43ba4674dd273aafdcfba3b
Carlos Mbendera <carlosmbendera@gmail.com>
2026-02-14 13:46:28 -0800

    Added Emotions From Speech Framework

--- CYT/CYTApp.swift
+++ CYT/CYTApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct CYTApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+           // ContentView()
+            TestSpeechEmotionsContentView()
         }
     }
 }
